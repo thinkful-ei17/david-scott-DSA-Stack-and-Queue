89f89e422803f9a095b9483296cd746a59b7ccf4
David Park <dysp92@gmail.com>
2018-03-02 08:27:24 -0800

    created pop and peek methods

--- stack.js
+++ stack.js
@@ -1,26 +1,49 @@
 class _Node {
-    constructor(data, next) {
-        this.data = data;
-        this.next = next;
-    }
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class Stack{
-    constructor(){
-        this.top = null;
+  constructor(){
+    this.top = null;
+  }
+
+  push(data){
+    if(this.top === null){
+      this.top = new _Node(data, null);
+      return this.top;
     }
 
-    push(data){
-        if(this.top === null){
-            this.top = new _Node(data, null);
-            return this.top;
-        }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
 
-        const node = new _Node(data, this.top);
-        this.top = node;
+  pop(){
+    const node = this.top;
+    this.top = this.top.next;
+    return node.data;
+  }
+
+  peek() {
+    if(this.top === null){
+      console.log('stack is empty!');
+      return;
     }
-
-    pop(){
-
-    }
+    console.log(this.top.data);
+  }
 }
+
+function main() {
+  let StarTrek = new Stack();
+
+  StarTrek.push('Kirk');
+  StarTrek.push('Spock');
+  StarTrek.push('McCoy');
+  StarTrek.push('Scotty');
+  console.log(StarTrek);
+  StarTrek.peek();
+}
+
+main();
