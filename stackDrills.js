f13e6b2593271de0580a1afdf2efc9b3bbd25073
David Park <dysp92@gmail.com>
2018-03-02 09:49:24 -0800

    started pseudocode for parens exercise

--- stackDrills.js
+++ stackDrills.js
@@ -1,10 +1,9 @@
 const Stack = require('./stack.js');
 
-let stack = new Stack();
 
 function is_palindrome(s) {
+  let stack = new Stack();
   s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  let newStr = '';
   //loop over string and push characters into stack;
   for(let i=0; i<s.length; i++) {
     stack.push(s[i]);
@@ -23,3 +22,13 @@ function is_palindrome(s) {
 console.log(is_palindrome('dad'));
 console.log(is_palindrome('A man, a plan, a canal: Panama'));
 console.log(is_palindrome('David'));
+
+
+// (1+4) - (8-4)
+// ((2+2) + (1+1)
+// declare a var, replacing all non parens
+// s = s.replace(/[^()])/g, '')
+
+function hi(s) {
+  s = s.replace(/[^()]/g, '');
+}
