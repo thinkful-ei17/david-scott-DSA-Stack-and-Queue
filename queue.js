de4b93318b226dd2afe8eb76db2c63e0d1da7b2c
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-04 09:39:08 -0800

    queue functioning properly

--- queue.js
+++ queue.js
@@ -13,41 +13,63 @@ class Queue{
     this.first = null;
     this.last = null;
   }
-
+  //prev first <-prev next <-prev last
   enqueue(data){
     const node = new _Node(data);
 
     if(this.first === null){
       this.first = node;
     }
- 
+
     if(this.last){
-      node.next = this.last;
-      this.last.prev = node;
+      this.last.next = node;
+      node.prev = this.last;
     }  
+
     this.last = node;
     return;
   }
+
   dequeue(){
     if(this.first === null){
       return null;
     }
-    const node = this.first;
-    this.first = this.first.prev;
 
-    if(node === this.last){
+    const nodeToDequeue = this.first;
+    this.first = this.first.next;
+
+    if(nodeToDequeue === this.last){
       this.last = null;
     }
 
-    return node.data;
+    return nodeToDequeue.data;
   }
 }
+
+//=========== ^ End of Class ^ ===============
+
 
 function peek(q){
   if(!q.first){
     return null;
   }
   return q.first.data;
+}
+
+function display(q){
+  if(!q.first){
+    console.log('no q');
+    return null;
+  }
+
+  let currNode = q.first;
+  console.log('-----------start');
+  while(currNode !== null){
+    console.log(currNode.data);
+    currNode = currNode.next;
+  }
+  console.log('------------end');
+  return;
 }
 
 const starTrekQ = new Queue();
@@ -61,4 +83,14 @@ function main(){
 }
 
 main();
-console.log(peek(starTrekQ));
+// console.log(peek(starTrekQ));
+// display(starTrekQ);
+// starTrekQ.dequeue();
+// display(starTrekQ);
+// starTrekQ.dequeue();
+// display(starTrekQ);
+// starTrekQ.dequeue();
+// display(starTrekQ);
+// starTrekQ.dequeue();
+// display(starTrekQ);
+console.log(starTrekQ.last);
